f8e00089bcf7f311c0eaf524319551baf0d1b944
Parker King <kingparker.a@gmail.com>
2022-01-05 15:48:50 -0700

    Create Presenter and Service layers for FeedFragment logic

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import edu.byu.cs.tweeter.client.backgroundTask.GetFeedTask;
 import edu.byu.cs.tweeter.client.backgroundTask.GetStoryTask;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.Status;
@@ -22,12 +23,52 @@ public class StatusService {
         void handleException(Exception ex);
     }
 
+    public void getFeed(AuthToken authToken, User targetUser,
+                        int limit, Status lastStatus, StatusService.Observer observer) {
+        GetFeedTask getFeedTask = new GetFeedTask(authToken, targetUser, limit,
+                lastStatus, new GetFeedHandler(observer));
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(getFeedTask);
+    }
+
     public void getStory(AuthToken authToken, User targetUser,
                          int limit, Status lastStatus, StatusService.Observer observer) {
         GetStoryTask getStoryTask = new GetStoryTask(authToken, targetUser, limit,
                 lastStatus, new GetStoryHandler(observer));
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(getStoryTask);
+    }
+
+    /**
+     * Message handler (i.e., observer) for GetFeedTask.
+     */
+    private class GetFeedHandler extends Handler {
+
+        private StatusService.Observer observer;
+
+        public GetFeedHandler(StatusService.Observer observer) {
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+
+            boolean success = msg.getData().getBoolean(GetFeedTask.SUCCESS_KEY);
+            if (success) {
+                List<Status> statuses = (List<Status>) msg.getData().getSerializable(GetFeedTask.STATUSES_KEY);
+                boolean hasMorePages = msg.getData().getBoolean(GetFeedTask.MORE_PAGES_KEY);
+
+                observer.handleSuccess(statuses, hasMorePages);
+            } else if (msg.getData().containsKey(GetFeedTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(GetFeedTask.MESSAGE_KEY);
+
+                observer.handleFailure(message);
+            } else if (msg.getData().containsKey(GetFeedTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(GetFeedTask.EXCEPTION_KEY);
+
+                observer.handleException(ex);
+            }
+        }
     }
 
     /**
