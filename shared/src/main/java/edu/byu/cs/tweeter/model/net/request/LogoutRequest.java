bd8ed03efde08874b965cd350e751949bf94ad4e
Parker King <kingparker.a@gmail.com>
2022-01-14 21:50:46 -0700

    Added constructor for client use

--- shared/src/main/java/edu/byu/cs/tweeter/model/net/request/LogoutRequest.java
+++ shared/src/main/java/edu/byu/cs/tweeter/model/net/request/LogoutRequest.java
@@ -1,4 +1,12 @@
 package edu.byu.cs.tweeter.model.net.request;
 
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+
 public class LogoutRequest extends AuthorizedRequest {
+
+    private LogoutRequest() {super();}
+
+    public LogoutRequest(AuthToken authToken, String currUserAlias) {
+        super(authToken, currUserAlias);
+    }
 }
