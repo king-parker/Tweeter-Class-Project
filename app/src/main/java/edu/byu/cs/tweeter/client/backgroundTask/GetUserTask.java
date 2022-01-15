afafc6c5c3113bb9746de40e0a5a127e36a7c6ec
Parker King <kingparker.a@gmail.com>
2022-01-14 21:52:17 -0700

    UserService tasks call server

--- app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/GetUserTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/GetUserTask.java
@@ -2,9 +2,16 @@ package edu.byu.cs.tweeter.client.backgroundTask;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
+import java.io.IOException;
+
+import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.model.net.TweeterRemoteException;
+import edu.byu.cs.tweeter.model.net.request.GetUserRequest;
+import edu.byu.cs.tweeter.model.net.response.GetUserResponse;
 
 /**
  * Background task that returns the profile for a specified user.
@@ -12,8 +19,11 @@ import edu.byu.cs.tweeter.model.domain.User;
 public class GetUserTask extends AuthorizedTask {
 
     private static final String LOG_TAG = "GetUserTask";
+    private static final String URL_PATH = "/user/get";
 
     public static final String USER_KEY = "user";
+
+    private GetUserRequest request;
 
     /**
      * Alias (or handle) for user whose profile is being retrieved.
@@ -28,13 +38,29 @@ public class GetUserTask extends AuthorizedTask {
         super(authToken, messageHandler);
 
         this.alias = alias;
+        Cache cache = Cache.getInstance();
+        this.request = new GetUserRequest(cache.getCurrUserAuthToken(), cache.getCurrUser().getAlias(), alias);
     }
 
     @Override
-    protected boolean runTask() {
-        this.selectedUser = getFakeData().findUserByAlias(alias);
+    protected boolean runTask() throws IOException, TweeterRemoteException {
+        try {
+            GetUserResponse response = getServerFacade().sendRequest(request, URL_PATH, GetUserResponse.class);
 
-        return true;
+            if (response.isSuccess()) {
+                this.selectedUser = response.getUser();
+
+                BackgroundTaskUtils.loadImage(selectedUser);
+                return true;
+            } else {
+                this.errorMessage = response.getMessage();
+                return false;
+            }
+        }
+        catch (Exception ex) {
+            Log.e(LOG_TAG, ex.getMessage(), ex);
+            throw ex;
+        }
     }
 
     @Override
--- app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/LogoutTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/LogoutTask.java
@@ -1,8 +1,15 @@
 package edu.byu.cs.tweeter.client.backgroundTask;
 
 import android.os.Handler;
+import android.util.Log;
 
+import java.io.IOException;
+
+import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.net.TweeterRemoteException;
+import edu.byu.cs.tweeter.model.net.request.LogoutRequest;
+import edu.byu.cs.tweeter.model.net.response.LogoutResponse;
 
 /**
  * Background task that logs out a user (i.e., ends a session).
@@ -10,13 +17,32 @@ import edu.byu.cs.tweeter.model.domain.AuthToken;
 public class LogoutTask extends AuthorizedTask {
 
     private static final String LOG_TAG = "LogoutTask";
+    private static final String URL_PATH = "/user/logout";
+
+    private LogoutRequest request;
 
     public LogoutTask(AuthToken authToken, Handler messageHandler) {
         super(authToken, messageHandler);
+
+        Cache cache = Cache.getInstance();
+        this.request = new LogoutRequest(cache.getCurrUserAuthToken(), cache.getCurrUser().getAlias());
     }
 
     @Override
-    protected boolean runTask() {
-        return true;
+    protected boolean runTask() throws IOException, TweeterRemoteException {
+        try {
+            LogoutResponse response = getServerFacade().sendRequest(request, URL_PATH, LogoutResponse.class);
+
+            if (response.isSuccess()) {
+                return true;
+            } else {
+                this.errorMessage = response.getMessage();
+                return false;
+            }
+        }
+        catch (Exception ex) {
+            Log.e(LOG_TAG, ex.getMessage(), ex);
+            throw ex;
+        }
     }
 }
